d69294d3627dc9c83e7c923d4f1cdcd29768c5de
Anthony Angel <aangel@gls.com>
2019-10-03 16:58:03 -0400

    directional keys added, lots of initial housekeeping added.

--- example1.py
+++ example1.py
@@ -1,9 +1,10 @@
 import pygame
 
+
 pygame.init()
 
 screenw = 800
-screenh = 600
+screenh = 800
 
 white = (255, 255, 255)
 black = (0, 0, 0)
@@ -17,18 +18,47 @@ pygame.display.set_caption('Slither')
 pygame.display.update()
 
 gameExit = False
+
+#variables for snake
+lead_x = 300
+lead_y = 300
+lead_x_change = 0
+lead_y_change = 0
+
+clock = pygame.time.Clock()
+
 while not gameExit:
     # event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                lead_x_change = -10
+                lead_y_change = 0
+            elif event.key == pygame.K_RIGHT:
+                lead_x_change = 10
+                lead_y_change = 0
+            elif event.key == pygame.K_UP:
+                lead_y_change = -10
+                lead_x_change = 0
+            elif event.key == pygame.K_DOWN:
+                lead_y_change = 10
+                lead_x_change = 0
 
-    # general game logic
-    gameDisplay.fill(green)
-    pygame.draw.rect(gameDisplay, black, [50, 50, 100, 100])
-    pygame.draw.rect(gameDisplay, red, [50, 50, 100, 10])
+    # snake logic
+    if lead_x > screenw or lead_x < 0 or lead_y > screenh or lead_y < 0:
+        gameExit = True
 
+    lead_x += lead_x_change
+    lead_y += lead_y_change
+
+    # color logic
+    gameDisplay.fill(white)
+    pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, screenw / 100, screenh / 100])
     pygame.display.update()
+
+    clock.tick(120)
 
 pygame.quit()
 quit()
